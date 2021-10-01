87d2587cd8b8f51d2d5df648f9211f4a60bddb26
saymarizvi <rizvisayma@yahoo.com>
2021-10-02 00:00:28 +0530

    added morgan for logging

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const planetsRouter = require('./routes/planets/planets.router');
 const cors = require('cors');
+const morgan = require('morgan');
 
 const app = express();
 const path = require('path');
 
 app.use(cors({
     origin: 'http://localhost:3000'
-}))
+}));
+
+app.use(morgan('combined'));
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname,'..','public')));
