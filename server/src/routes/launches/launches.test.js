eb5938576ba2a0aa6f5884aad95b5ea633146410
saymarizvi <rizvisayma@yahoo.com>
2021-10-02 23:34:16 +0530

    adding test cases for get launches

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,27 @@
+const request = require('supertest');
+const app = require('../../app');
+
+describe('TEST GET /launches', () => {
+    test('It should respond with 200 success', async() => {
+       const response  = await request(app)
+       .get('/launches')
+       .expect('Content-Type',/json/)
+       .expect(200);
+  
+    });
+});
+
+describe('TEST POST /launches', () => {
+    test('It should respond with 200 success', () => {
+       const response  = 200;
+       expect(response).toBe(200);
+    });
+
+    test('It should catch missing required properties',()=>{
+
+    });
+
+    test('It should catch invalid date',()=>{
+
+    });
+});
